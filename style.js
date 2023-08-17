1b9fa9ef58ea5c7a026f03427af0e3188c66fd25
Aashlesha07 <kharunkaraashlesha07@gmail.com>
2023-08-17 18:53:04 +0530

    added

---
+++ style.js
@@ -0,0 +1,34 @@
+var tableskill = document.getElementsByClassName("skillname");
+var educationtable = document.getElementsByClassName("tableeducation");
+
+function opentab(tabname){
+    for(tableskill of tableskill){
+        tableskill.classList.remove("skillactive");
+    }
+    for(educationtable of educationtable){
+        educationtable.classList.remove("ted");
+    }
+    event.currentTarget.classList.add("skillactive");
+    document.getElementById(tabname).classList.add("ted");
+}
+
+function sendMail(){
+    (function(){
+        emailjs.init("I0KjoTLZEbzVf2V-4");
+    })();
+
+    var params = {
+        name: document.querySelector("#name").value,
+        gemail: document.querySelector("#gemail").value, 
+        mobilenumber: document.querySelector("#mobilenumber").value,
+        message: document.querySelector("#message").value,
+    };
+    var serviceID = "service_n65g0jg";
+    var templateID = "template_gtti2fp";
+
+    emailjs.send(serviceID, templateID, params)
+    .then( res => {
+        alert("Email send successfully !");
+    })
+    .catch();
+}
